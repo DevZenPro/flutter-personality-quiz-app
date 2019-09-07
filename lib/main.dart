abb8e113cbfb092f7089127d6f6745fcca0cf27f
Alvi Akbar <alvi.akbar@pushinteractions.com>
2019-09-07 10:10:04 -0600

    Created three raised buttons widget and set onPressed to null

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My Awesome App')),
-        body: Text('This is my default text! :D'),
+        body: Column(children: [
+          Text('The question is '),
+          RaisedButton(
+            child: Text(
+              'Answer 1',
+            ),
+            onPressed: null
+          ),
+          RaisedButton(
+            child: Text(
+              'Answer 2',
+            ),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text(
+              'Answer 3',
+            ),
+            onPressed: null,
+          ),
+        ]),
       ),
     );
   }
