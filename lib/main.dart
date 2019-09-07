d4690700aa0d5c61e8fa3201720a35b8cd49da69
Alvi Akbar <alvi.akbar@pushinteractions.com>
2019-09-07 10:54:33 -0600

    Created a list questions that represents collection of all questions

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext contexts) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My Awesome App')),
         body: Column(children: [
-          Text('The question is '),
+          Text(questions[0]),
           RaisedButton(
               child: Text(
                 'Answer 1',
